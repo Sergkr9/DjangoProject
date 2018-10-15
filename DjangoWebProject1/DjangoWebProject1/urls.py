6ba3475ba16fd60aefd2b3fddccbce552df51a50
Sergey Kryuchkov <sergkr9@live.ru>
2018-10-15 11:41:23 +0300

    test1

--- DjangoWebProject1/DjangoWebProject1/urls.py
+++ DjangoWebProject1/DjangoWebProject1/urls.py
@@ -4,18 +4,9 @@ Definition of urls for DjangoWebProject1.
 
 from django.conf.urls import include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+import DjangPr.views
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', DjangoWebProject1.views.home, name='home'),
-    # url(r'^DjangoWebProject1/', include('DjangoWebProject1.DjangoWebProject1.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-]
+    url (r'^$', DjangPr.views.index, name ='index'),
+    url (r'^home$', DjangPr.views.index, name ='home')
+    ]
